d60f19c1d6f6e7b46b61fc708c339f37b10df381
avandefeniks <angel.vandefeniks@gmail.com>
2022-05-03 16:22:00 -0700

    added second js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,13 @@
 artistEl = document.querySelector(".artist");
 searchButtonEl = document.querySelector(".search-btn");
+songList = document.querySelector(".song-list");
 
 function formHandler(event) {
     // keep page from refreshing 
     event.preventDefault();
 
     var artistInput = artistEl.value;
+    // console.log(artistInput);
     
     getDiscography(artistInput);
 
@@ -15,12 +17,10 @@ function getDiscography(artist) {
     var APIkey = "2272bb113a5e5a54f0040d944c8e7d08"
     
     // check for spaces in the name and add a "+"
-    console.log(APIurl)
-
     if (artist.match(/\s/)) {
         var a = artist.split(' ').join('+');
         var APIurl = "http://api.musixmatch.com/ws/1.1/track.search?apikey=" + APIkey + "&q_artist=" + a + "&page_size=3&page=1&s_track_rating=desc";
-        console.log(APIurl);
+        // console.log(APIurl);
         
     } else {
         var APIurl = "http://api.musixmatch.com/ws/1.1/track.search?apikey=" + APIkey + "&q_artist=" + artist + "&page_size=3&page=1&s_track_rating=desc";
@@ -31,19 +31,31 @@ function getDiscography(artist) {
             alert("Please enter a valid artist!");
         } else if (response.ok) {
             response.json().then(function(data) {
-                var trackList = data.message.body.track_list[0].track.track_name
-                console.log(trackList);
+                var trackListArr = data.message.body.track_list;
+                console.log(trackListArr);
                 // create for loop
-                // create counter
-                // create element "a"
-                // add class list with UIkit attributes
-                // add href with data track list. make inex dynamic 
-                // add attribute to launch lyrics in new window (*, _target)
+                for (var i = 0; i < trackListArr.length; i++) {
+                    var trackList = data.message.body.track_list[i].track.track_name;
+                    // replace spaces in song title with +
+                    if (trackList.match(/\s/)) {
+                        var a = trackList.split(' ').join('+');
+                    }
+                
+                    console.log(trackList);
+                    // create list elements
+                    var listEl = document.createElement("li");
+                    // listEl.classList = "list-item";
+                    listEl.innerHTML = "<a href=./secondary.html?artist=" + artist + "&song=" + a + ">" + trackList + "</a>";
+                    songList.appendChild(listEl);
+                    // console.log(listEl);
+                }
+            
+                // add attribute to launch lyrics in new window (target, _target)
                 // create span element to to hold every thing
                 // add it to the list
                 // append to dom container
-                // increment index count by one
-            })
+                
+            });
         } else {
             alert("Error: " + response.statusText);
         }
@@ -53,6 +65,6 @@ function getDiscography(artist) {
     });
 }
 
+if (searchButtonEl) {
 searchButtonEl.addEventListener("click", formHandler);
-
-// q_artist=justin%20bieber
+}
---
+++ assets/js/secondary.js
@@ -0,0 +1,13 @@
+
+
+function getArtistAndSong () {
+    // get url
+    var url = document.location.search;
+
+    // get artist name
+    artistName = url.split("=")[1];
+
+    // get song
+    songTitle = url.split("=")[2];
+    console.log(artistName, songTitle);
+};
