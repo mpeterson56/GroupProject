e19fbbb77591507e61d53e0f299f99d5ae15360d
Mike Peterson <mikemikemike3390@yahoo.com>
2022-05-04 18:13:05 -0700

    final step to restore deleted work

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,11 +19,11 @@ function getDiscography(artist) {
     // check for spaces in the name and add a "+"
     if (artist.match(/\s/)) {
         var a = artist.split(' ').join('+');
-        var APIurl = "http://api.musixmatch.com/ws/1.1/track.search?apikey=" + APIkey + "&q_artist=" + a + "&page_size=10&page=1&s_track_rating=desc";
+        var APIurl = "http://api.musixmatch.com/ws/1.1/track.search?apikey=" + APIkey + "&q_artist=" + a + "&page_size=3&page=1&s_track_rating=desc";
         // console.log(APIurl);
         
     } else {
-        var APIurl = "http://api.musixmatch.com/ws/1.1/track.search?apikey=" + APIkey + "&q_artist=" + artist + "&page_size=10&page=1&s_track_rating=desc";
+        var APIurl = "http://api.musixmatch.com/ws/1.1/track.search?apikey=" + APIkey + "&q_artist=" + artist + "&page_size=3&page=1&s_track_rating=desc";
     }
     
     fetch(APIurl).then(function(response) {
@@ -38,14 +38,14 @@ function getDiscography(artist) {
                     var trackList = data.message.body.track_list[i].track.track_name;
                     // replace spaces in song title with +
                     if (trackList.match(/\s/)) {
-                        var tracks = trackList.split(' ').join('+');
+                        var a = trackList.split(' ').join('+');
                     }
                 
                     console.log(trackList);
                     // create list elements
                     var listEl = document.createElement("li");
                     // listEl.classList = "list-item";
-                    listEl.innerHTML = "<a href=./secondary.html?artist=" + a + "&song=" + tracks + ">" + trackList + "</a>";
+                    listEl.innerHTML = "<a href=./secondary.html?artist=" + artist + "&song=" + a + ">" + trackList + "</a>";
                     songList.appendChild(listEl);
                     // console.log(listEl);
                 }
@@ -64,29 +64,6 @@ function getDiscography(artist) {
         alert("Can't access track list!");
     });
 }
-
-function getArtistName () {
-    var artistName = document.location.search;
-    var url = document.location.search.includes("?");
-
-
-    // check for query string
-    if (url) {
-        // get artist name from query string pass it to lookup function
-        var artistNameArr = artistName.split("=");
-        var artist = artistNameArr[1];
-        getDiscography(artistName);
-        console.log(artist);
-    }
-    else {
-        console.log("getArtistName had 0 results")
-        return;
-        
-    }
-
-}
-
-getArtistName();
 
 if (searchButtonEl) {
 searchButtonEl.addEventListener("click", formHandler);
